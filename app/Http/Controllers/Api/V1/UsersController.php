4aeae8c3d12c305f051fe1301e1598dac94fad61
HASNAIN123 <107262800+Falil1@users.noreply.github.com>
2023-04-02 13:05:40 +0800

    Email not need to be email

--- app/Http/Controllers/Api/V1/UsersController.php
+++ app/Http/Controllers/Api/V1/UsersController.php
@@ -30,7 +30,7 @@ public function register(Request $request)
 {
     $request->validate([
         'name' => 'required|string|max:255',
-        'email' => 'required|string|email|max:255|unique:users',
+        'email' => 'required|string|max:255|unique:users',
         'password' => 'required|string|min:8|confirmed',
     ]);
 
