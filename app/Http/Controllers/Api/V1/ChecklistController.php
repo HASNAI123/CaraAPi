8212bb6e8791e4a28dec3b2e76814dd4a7f8e587
Hasnain <Hasnainrashid1234@gmail.com>
2023-12-06 15:32:30 +0800

    checklist changes

--- app/Http/Controllers/Api/V1/ChecklistController.php
+++ app/Http/Controllers/Api/V1/ChecklistController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Securitymaxvalue;
 use App\Models\RemarkSAB;
+use App\Models\RemarkSA;
 
 class ChecklistController extends Controller
 {
