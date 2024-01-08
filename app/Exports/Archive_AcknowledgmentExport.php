b8737e715cdfc6974dbb859b478cee23faac7bc5
Hasnain <Hasnainrashid1234@gmail.com>
2024-01-08 16:33:33 +0800

    Set sequence

--- app/Exports/Archive_AcknowledgmentExport.php
+++ app/Exports/Archive_AcknowledgmentExport.php
@@ -10,21 +10,19 @@ class Archive_AcknowledgmentExport implements FromQuery, WithHeadings
 {
     public function query()
     {
-        return AcknowledgmentArchive::query()->select('Sop_title',  'user_name', 'User_id', 'Terms_1', 'Terms_2', 'Date_Downloaded', 'Type');
+        return AcknowledgmentArchive::query()->select('Sop_title', 'Type', 'user_name', 'User_id', 'Terms_1', 'Terms_2', 'Date_Downloaded');
     }
 
     public function headings(): array
     {
         return [
             'Sop_title',
+            'SOP Type',
             'Employee Name',
             'Employee id',
             'Terms 1',
             'Terms 2',
-            'Date Downloaded',
-            'SOP Type',
-            'created_at',
-            'updated_at',
+            'Date Downloaded'
         ];
     }
 
